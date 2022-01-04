c98ceba5ea85fab4a9b7d99f7290b45ad4814ecd
sprabesh122 <88088638+sprabesh122@users.noreply.github.com>
2022-01-05 00:31:40 +0545

    initial upload

---
+++ main.js
@@ -0,0 +1,90 @@
+// Input event is used to show change at the particular moment
+
+var cel = document.getElementById('cel');
+var fah = document.getElementById('fah');
+var kel = document.getElementById('kel');
+
+// For fahrenheit part
+
+cel.addEventListener('input', function(){
+
+    let c = this.value // storing the value
+
+    // Converting to Fahrenheit
+
+    let f = (c * 9/5) + 32;
+
+    // Checking for number of decimal places
+
+    if(!Number.isInteger(f)){
+        f = f.toFixed(4);
+    }
+
+    // Changing the value of fah
+
+    fah.value = f;
+
+    // Converting to Kelvin
+
+    let k = (c * 1) + 273.15;
+
+    if(!Number.isInteger(k)){
+        k = k.toFixed(4);
+    }
+
+    kel.value = k;
+
+});
+
+// For celsius part
+
+fah.addEventListener('input', function(){
+
+    let f = this.value; // storing the value
+
+    let c = (f - 32) * 5/9; // converting to celsius
+
+    // Checking for number of decimal places
+
+    if(!Number.isInteger(c)){
+        c = c.toFixed(4);
+    }
+ 
+    cel.value = c; // changing to celsius
+
+    let k =   (f - 32) * 5/9 + 273.15;
+
+    if(!Number.isInteger(k)){
+        k = k.toFixed(4);
+    }
+
+    kel.value = k;
+
+});
+
+// For kelvin part
+kel.addEventListener('input', function(){
+     
+    let k = this.value; 
+
+    // Converting kelvin to celsius
+
+    let c = (k - 273.15);
+
+    if(!Number.isInteger(c)){
+        c = c.toFixed(4);
+    }
+
+    cel.value = c;
+
+    // Converting kelvin to fahrenheit
+
+    let f = (k - 273.15) * 9/5 + 32;
+
+    if(!Number.isInteger(f)){
+        f = f.toFixed(4);
+    }
+
+    fah.value = f;
+
+});
